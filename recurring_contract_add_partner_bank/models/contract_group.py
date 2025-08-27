72bb3936b70e1bd847352d7d0151a80302986ea0
David Wulliamoz <dwulliamoz@compassion.ch>
2025-08-27 11:29:56 +0200

    fix

--- recurring_contract_add_partner_bank/models/contract_group.py
+++ recurring_contract_add_partner_bank/models/contract_group.py
@@ -19,14 +19,15 @@ class ContractGroup(models.Model):
     def _build_invoice_gen_data(
         self, invoicing_date, invoicer, gift_wizard=False
     ):
-        inv_data = super()._build_invoice_gen_data(invoicing_date, gift_wizard,gift_wizard)
-        if self.ref[-1:]==mod10r(self.ref[:-1]):
-            bank_id=self.payment_mode_id.fixed_journal_id.bank_account_id
-        else:
-            bank_id = self.payment_mode_id.fixed_journal_id.bank_account_id
-        inv_data.update(
-            {
-                "partner_bank_id": bank_id,
-            }
-        )
-
+        inv_data = super()._build_invoice_gen_data(invoicing_date, invoicer,gift_wizard)
+        if self.payment_mode_id.bank_account_link=="fixed":
+            if self.ref[-1:]==mod10r(self.ref[:-1]):
+                bank_id=self.payment_mode_id.fixed_journal_id.bank_account_id
+            else:
+                bank_id = self.payment_mode_id.fixed_journal_id.bank_account_id
+            inv_data.update(
+                {
+                    "partner_bank_id": bank_id,
+                }
+            )
+        return inv_data
