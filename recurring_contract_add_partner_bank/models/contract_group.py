280dd22bc1d06169856f7c31c64f12eeecece4c2
David Wulliamoz <dwulliamoz@compassion.ch>
2025-08-27 11:19:25 +0200

    Add partner bank id in inv generation

---
+++ recurring_contract_add_partner_bank/models/contract_group.py
@@ -0,0 +1,32 @@
+##############################################################################
+#
+#    Copyright (C) 2014-2025 Compassion CH (http://www.compassion.ch)
+#    Releasing children from poverty in Jesus' name
+#    @author: David Wulliamoz
+#
+#    The licence is in the file __manifest__.py
+#
+##############################################################################
+
+from odoo import fields, models
+from odoo.tools.misc import mod10r
+
+
+class ContractGroup(models.Model):
+    _inherit = "recurring.contract.group"
+
+
+    def _build_invoice_gen_data(
+        self, invoicing_date, invoicer, gift_wizard=False
+    ):
+        inv_data = super()._build_invoice_gen_data(invoicing_date, gift_wizard,gift_wizard)
+        if self.ref[-1:]==mod10r(self.ref[:-1]):
+            bank_id=self.payment_mode_id.fixed_journal_id.bank_account_id
+        else:
+            bank_id = self.payment_mode_id.fixed_journal_id.bank_account_id
+        inv_data.update(
+            {
+                "partner_bank_id": bank_id,
+            }
+        )
+
